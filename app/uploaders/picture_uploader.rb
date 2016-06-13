a4ce870a5dbf8823dca6a6766dcaa2baab9203c7
Cody Condon <codycondon50@gmail.com>
2016-06-12 22:17:29 -0400

    Update error in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production
+  if Rails.env.production?
     storage :fog
   else
     storage :file
